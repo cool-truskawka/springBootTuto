a3098abb0f94a40e820ef28aeda6c79292d45850
cool-truskawka <dairaksass@gmail.com>
2023-02-20 19:49:04 +0100

    initial commit

---
+++ src/main/java/com/example/springBootTuto/SpringBootTutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springBootTuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springBootTuto/SpringBootTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBootTuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
